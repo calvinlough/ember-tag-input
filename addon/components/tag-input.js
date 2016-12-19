19bdb8acf0a644134b4015e1da275ee4f522b7e0
Calvin Lough <calvinlough@gmail.com>
2016-12-19 18:55:10 -0500

    Get it rendering in the dummy app

---
+++ addon/components/tag-input.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../templates/components/tag-input';
+
+export default Ember.Component.extend({
+  layout,
+  tagName: 'span'
+});
---
+++ app/components/tag-input.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-tag-input/components/tag-input';
