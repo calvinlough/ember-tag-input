206cddb11c72cff46582760c63dba878a6ff2d72
Calvin Lough <clough@freshbooks.com>
2020-01-03 17:22:43 -0500

    Update ember-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -11,14 +11,10 @@ module.exports = function() {
     return {
       scenarios: [
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-          },
+          name: 'ember-lts-3.15',
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
+              'ember-source': '~3.15.0'
             }
           }
         },
