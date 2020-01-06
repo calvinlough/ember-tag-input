6f64057bd899fe7f1e2df1abc7902732c49b3630
Calvin Lough <clough@freshbooks.com>
2020-01-06 09:28:38 -0500

    Add targets file, remove polyfill

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,11 +2,7 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    'ember-cli-babel': {
-      includePolyfill: true
-    },
-  });
+  var app = new EmberAddon(defaults, {});
 
   /*
     This build file specifies the options for the dummy test app of this
