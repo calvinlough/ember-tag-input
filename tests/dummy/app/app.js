7f71feefae3adb84bb913de5ab43ba6d143f0b1d
Charles Fries <charliefries@icloud.com>
2021-09-14 17:47:11 -0700

    Rm MODEL_FACTORY_INJECTIONS
    https://deprecations.emberjs.com/v2.x/#toc_ember-model_factory_injections-removed

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -3,9 +3,6 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
 
-// TODO:
-// Ember.MODEL_FACTORY_INJECTIONS = true;
-
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
